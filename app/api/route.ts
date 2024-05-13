3d16d2d0cf07bad1d198920e1408d9eb88981e64
tanadon <tanadon@woxacorp.com>
2024-05-13 11:21:37 +0700

    update css and some logic

--- app/api/route.ts
+++ app/api/route.ts
@@ -2,7 +2,6 @@ import fs from "fs";
 
 export async function POST(req: Request, res: Response) {
   const body = await req.json();
-  console.log(body.data);
   const filePath = `./output/${body.filename}.ts`; // Path to the file
 
   fs.writeFile(filePath, body.data, (err) => {
--- app/page.tsx
+++ app/page.tsx
@@ -5,21 +5,29 @@ import { json2ts } from "json-ts";
 
 export default function Home() {
   const [file, setFile] = useState([]);
+  const [fileName, setFileName] = useState("");
+  const [objectName, setObjectName] = useState("");
 
   const setFileHandler = (e) => {
-    const file = e.target.files?.[0];
+    setFile(e.target.files?.[0]);
+  };
+
+  const generate = () => {
     if (file) {
       const reader = new FileReader();
       reader.onload = (event) => {
         const contents = event.target?.result as string;
-        const body = JSON.parse(contents).res_body.data;
-        const bodyTs = json2ts(JSON.stringify(body));
+        const body = JSON.parse(contents);
+        const bodyTs = json2ts(JSON.stringify(body), { rootName: objectName });
         fetch("/api", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
           },
-          body: JSON.stringify({ data: bodyTs, filename: "test" }),
+          body: JSON.stringify({
+            data: bodyTs,
+            filename: fileName,
+          }),
         })
           .then((response) => response.json())
           .then((data) => console.log(data))
@@ -30,18 +38,32 @@ export default function Home() {
   };
 
   return (
-    <>
+    <div style={{ color: "black" }}>
+      <input
+        placeholder="file name"
+        onChange={(e) => setFileName(e.target.value)}
+      />
+      <br />
+      <br />
+      <input
+        placeholder="object name"
+        onChange={(e) => setObjectName(e.target.value)}
+      />
+      <br />
+      <br />
       <input
         type="file"
         id="files"
         name="files"
         accept=".json"
-        multiple
-        onChange={setFileHandler}
+        // multiple
+        onChange={(e) => {
+          setFileHandler(e);
+        }}
       />
       <br />
       <br />
-      <input type="submit" />
-    </>
+      <button onClick={generate}>Generate</button>
+    </div>
   );
 }
